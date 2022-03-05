daf3439bb5c1013a4d04a97b812b755edd03a6e6
Nyadav8 <navneetsingh.d.great@gmail.com>
2022-03-06 01:10:20 +0530

    Updates

---
+++ precache-manifest.50ba4e4613d64493d03ceca25cc03a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0043557be619d01c2d2c",
+    "url": "/nameIT/static/css/main.79ee1d65.chunk.css"
+  },
+  {
+    "revision": "0043557be619d01c2d2c",
+    "url": "/nameIT/static/js/main.0043557b.chunk.js"
+  },
+  {
+    "revision": "a21d76fba7646f42a4ca",
+    "url": "/nameIT/static/js/1.a21d76fb.chunk.js"
+  },
+  {
+    "revision": "2dc984200bf591100594",
+    "url": "/nameIT/static/js/runtime~main.2dc98420.js"
+  },
+  {
+    "revision": "3d008486c63d3c81890c157759e1c494",
+    "url": "/nameIT/index.html"
+  }
+];
